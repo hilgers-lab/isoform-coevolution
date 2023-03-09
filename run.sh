1efe37a0366f0b9944e9a7da8138523b715942f5
David Koppstein <koppstein@ie-freiburg.mpg.de>
2023-03-09 14:31:44 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ eval "$(conda shell.bash hook)"
 conda activate snakemake
 #snakemake --profile ~/.config/mpi-ie-slurm -j32
 #snakemake --configfile config/config.yaml --profile ~/.config/mpi-ie-slurm -j32
-snakemake --configfile config/config.yaml --profile ~/.config/mpi-ie-slurm-deep17 -j2 flags/all.done
+#snakemake --configfile config/config.yaml --profile ~/.config/mpi-ie-slurm-deep17 -j2 flags/all.done
 #snakemake --configfile config/config.yaml --profile ~/.config/mpi-ie-slurm-deep17 -j2 output/coevo-box/label-dominant_gene-FBgn0014010/corrected_mutinfo_clustermap.png
+snakemake --configfile config/config.yaml -j2 flags/all.done
