16a29efa08430d692a89c04072b663a856a5e470
David Koppstein <koppstein@ie-freiburg.mpg.de>
2023-03-09 14:42:44 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-eval "$(conda shell.bash hook)"
-conda activate snakemake
+#eval "$(conda shell.bash hook)"
+#conda activate snakemake
 #snakemake --profile ~/.config/mpi-ie-slurm -j32
 #snakemake --configfile config/config.yaml --profile ~/.config/mpi-ie-slurm -j32
 #snakemake --configfile config/config.yaml --profile ~/.config/mpi-ie-slurm-deep17 -j2 flags/all.done
